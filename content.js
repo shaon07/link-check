30fdd8671b8334d64a4ebd6cbac893df8a4e866d
Shaon07 <alishaon77@gmail.com>
2023-09-11 23:50:45 +0600

    [add] slice all info after the domain name then storing it

--- content.js
+++ content.js
@@ -1,35 +1,44 @@
-
-
-  
-  // content.js
+// content.js
 function normalizeURL(url) {
-    // Remove "www." and other variations, and convert to lowercase
-    url = url.replace(/^(https?:\/\/)?(www\d?\.)?/i, "").toLowerCase();
+    // Use the URL object to extract the domain
+    const urlObj = new URL(url);
+    const domain = urlObj.hostname.toLowerCase();
   
-    // Remove trailing slashes
-    url = url.replace(/\/$/, "");
+    // Create a new URL with just the protocol and domain
+    const truncatedURL = urlObj.protocol + '//' + domain;
   
-    return url;
+    return truncatedURL;
   }
   
   const currentURL = window.location.href;
-  const normalizedURL = normalizeURL(currentURL);
+  const truncatedURL = normalizeURL(currentURL);
   
   chrome.storage.sync.get("urls", function (data) {
     const savedURLs = data.urls || [];
-    const normalizedSavedURLs = savedURLs.map(normalizeURL);
-    if (!normalizedSavedURLs.includes(normalizedURL)) {
-      savedURLs.push(currentURL);
-      chrome.storage.sync.set({ urls: savedURLs });
-      // Inject the URL into the DOM (you can customize this)
+  
+    // Check if the truncated URL is in the list of saved URLs
+    if (!savedURLs.includes(truncatedURL)) {
+      // Inject the URL into the DOM with custom CSS styles
       const div = document.createElement("div");
       div.innerText = "You have visited this page before.";
-      div.style.cssText += 'position: sticky; background-color: red; bottom: 20px; z-index: 9999999999999999999999; padding: 10px; color: white; border-radius: 10px; width: 300px; left: 20px;';
       div.setAttribute("id", "visited-text"); // Add an ID for easy removal
+  
+      // Apply CSS styles
+      div.style.cssText +=
+        'position: sticky; background-color: red; bottom: 20px; z-index: 9999999999999999999999; padding: 10px; color: white; border-radius: 10px; width: 300px; left: 20px;';
+  
       document.body.appendChild(div);
+  
+      // Save the truncated URL to prevent re-injection
+      savedURLs.push(truncatedURL);
+      chrome.storage.sync.set({ urls: savedURLs }, function () {
+        // Handle errors if necessary
+        if (chrome.runtime.lastError) {
+          console.error(chrome.runtime.lastError);
+        }
+      });
     }
   });
-  
   // Listen for changes in storage (URL removal)
   chrome.storage.onChanged.addListener(function (changes, namespace) {
     if (namespace === "sync" && "urls" in changes) {
